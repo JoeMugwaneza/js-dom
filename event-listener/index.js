8690ac13feed6a9ef2d80c050c452928609cdae7
Mugwaneza Joseph <muganezajoseph@gmail.com>
2021-10-18 06:01:18 +0200

    added event listener both inline and event listeners

---
+++ event-listener/index.js
@@ -0,0 +1,13 @@
+let events = document.getElementById("events");
+let blueBtn = document.getElementById("blue");
+let redBtn = document.getElementById("red");
+
+// blueBtn.addEventListener('click', changeColorBlue)
+function changeColorBlue(){
+    events.style.color = 'blue';
+}
+
+// redBtn.addEventListener('click', changeColorRed);
+function changeColorRed(){
+    events.style.color = 'red';
+}
