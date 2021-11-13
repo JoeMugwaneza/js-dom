b9df3043e38f97138ce4e46b7bb09548f3dc9c5c
Mugwaneza Joseph <muganezajoseph@gmail.com>
2021-11-13 05:09:15 +0200

    managed to interact with other event types like focus, input, copy and paste

--- form/app.js
+++ form/app.js
@@ -3,4 +3,20 @@ let outcome = document.getElementById('outcome');
 document.querySelector('form').addEventListener('submit', (e) =>{
     outcome.textContent = `A submit event has been fired at ${e.timeStamp}`,
     e.preventDefault();
+    document.forms[0].name.value = "";
+})
+
+// time to play around with different event types
+
+let nameText = document.querySelector('input[type="text"]');
+
+nameText.addEventListener('input', () => {
+    outcome.textContent = `The value entered is ${nameText.value}`;
+})
+
+// Focus
+
+nameText.addEventListener('focus', (e) => {
+    e.target.style.backgroundColor = "lightblue";
+    e.target.style.color = "black";
 })
